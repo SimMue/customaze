4ed65b7efdad4099855441542d9242ab97f8b80a
Simon Müller <simonmueller1@hotmail.com>
2021-04-03 15:07:22 +0200

    chore: run prettier format

--- api/src/entities/entities.module.ts
+++ api/src/entities/entities.module.ts
@@ -3,6 +3,6 @@ import { EntitiesService } from './entities.service';
 import { EntitiesResolver } from './entities.resolver';
 
 @Module({
-  providers: [EntitiesResolver, EntitiesService]
+  providers: [EntitiesResolver, EntitiesService],
 })
 export class EntitiesModule {}
